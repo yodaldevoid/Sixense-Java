d4ae3d4572a5b068aea3bdb5be306a75cac20f45
Yoda12999 <ga29smith@yahoo.com>
2013-06-10 09:56:06 -0400

    Added license - closes #3

--- src/java/com/sixense/utils/enums/EnumActionType.java
+++ src/java/com/sixense/utils/enums/EnumActionType.java
@@ -1,7 +1,24 @@
+/*
+* This file is part of Sixense Java.
+*
+* Copyright © 2012-2013, Sixense Java Contributors
+* FTB Launcher is licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package com.sixense.utils.enums;
 
 /**
- * @author Yoda12999
+ * @author Gabriel Smith
  */
 public enum EnumActionType {
     BUTTON_PRESS,
